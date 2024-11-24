7c2cc751e82e69650eced6a6ff1aaf32561ebcfd
FTahmina <fencyworld701@gmail.com>
2024-11-24 21:26:06 +0600

    Add files via upload

--- myFile_1.py
+++ myFile_1.py
@@ -1,19 +1,29 @@
+import os
 import streamlit as st
+from PIL import Image
+
+cwd = os.getcwd()
+
+image_file = ('pic_1.PNG')
+image_path = os.path.join(cwd, image_file)
+img = Image.open(image_path)
+st.image(img)
 
 st.title('Streamlit')
-st.header("How to Create Your Own App!")
-
-#click_button = st.button("Click here")
+st.header('How to Create Your Own App!')
+st.write('')
 
 st.info('Go to GitHub.com')
 st.link_button('GitHub', 'https://github.com/')
 
 st.write('- Login / Create an Account')
-st.write('- Create a Public Repository > Add files > Upload File')
+st.write('- Create a New Repository > Add: Repository Name > Settings: Public > Create Repository') 
+st.write('- Add files > Upload File')
 st.write('- Upload your local .py file > Commit Changes')
+st.write('')
 
 st.info('Go to streamlit.app')
-st.link_button("Streamlit App", 'https://share.streamlit.io/')
+st.link_button('Streamlit App', 'https://share.streamlit.io/')
 st.write('- Login / Create an Account')
 st.write('- Create App > Connect to GitHub')
 st.write('- Select --> Deploy a public App from GitHub')
@@ -23,13 +33,22 @@ st.write('-- Main File Path --> Select your main .py file name')
 st.write('-- App URL --> Write an URL for your Website')
 st.write('- Deploy')
 st.write('')
-st.write('')
-st.write('')
+
 
 st.success('Your Website has been deployed successfully!')
 st.write('')
 st.write('')
+st.subheader('Update Your App')
+st.write('- Go to your GitHub Repository')
+st.write('- Add files > Upload File')
+st.write('- Upload your updated local .py file > Commit Changes')
+st.write('')
+st.write('')
 st.write('')
 
-st.write('For more infomation, please visit https://docs.streamlit.io/')
-st.snow()
+st.divider()
+st.write('R E F E R E N C E S')
+st.caption('- Streamlit Blog: https://blog.streamlit.io/host-your-streamlit-app-for-free/')
+st.caption('- Streamlit Documentation: https://docs.streamlit.io/')
+
+st.feedback('stars')
